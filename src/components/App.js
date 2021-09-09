7e983d8546de27807d71305f638d8e7810e8a3bc
Khnykin Andrey <beyondmyspace@yandex.ru>
2021-09-09 23:11:16 +0300

    added 11 sprint files

---
+++ src/components/App.js
@@ -0,0 +1,152 @@
+import React from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+import Footer from "./Footer";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import api from "../utils/Api";
+import EditProfilePopup from "./EditProfilePopup";
+import EditAvatarPopup from "./EditAvatarPopup";
+import AddPlacePopup from "./AddPlacePopup";
+
+function App() {
+  const [currentUser, setCurrentUser] = React.useState({});
+
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
+    React.useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] =
+    React.useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
+
+  const [selectedCard, setSelectedCard] = React.useState({});
+  const [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      .then(([userData, cards]) => {
+        setCurrentUser(userData);
+        setCards(cards);
+      })
+      .catch((e) => console.log(`Ошибка при получении данных: ${e}`));
+  }, []);
+
+  function handleEditAvatarClick() {
+    setIsEditAvatarPopupOpen(true);
+  }
+
+  function handleEditProfileClick() {
+    setIsEditProfilePopupOpen(true);
+  }
+
+  function handleAddCardClick() {
+    setIsAddPlacePopupOpen(true);
+  }
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  }
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    api
+      .setLike(card._id, !isLiked)
+      .then((newCard) => {
+        setCards((cards) =>
+          cards.map((c) => (c._id === card._id ? newCard : c))
+        );
+      })
+      .catch((err) => console.log(`Ошибка при попытке поставить лайк: ${err}`));
+  }
+
+  function handleCardDelete(card) {
+    api
+      .deleteCard(card._id)
+      .then(() => {
+        setCards(cards.filter((c) => c._id !== card._id));
+        closeAllPopups();
+      })
+      .catch((err) => console.log(`Ошибка при удалении карточки: ${err}`));
+  }
+
+  function handleUpdateUser(userData) {
+    api
+      .getUserData(userData)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) =>
+        console.log(`Ошибка при обновлении пользователя: ${err}`)
+      );
+  }
+
+  function handleUpdateAvatar(userData) {
+    api
+      .changeAvatar(userData)
+      .then((res) => {
+        setCurrentUser(res);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(`Ошибка при обновлении аватара: ${err}`));
+  }
+
+  function handleAddPlace(data) {
+    api
+      .addCard(data)
+      .then((newCard) => {
+        setCards([newCard, ...cards]);
+        closeAllPopups();
+      })
+      .catch((err) => console.log(`Ошибка при добавлении карточки: ${err}`));
+  }
+
+  function closeAllPopups() {
+    setIsEditAvatarPopupOpen(false);
+    setIsEditProfilePopupOpen(false);
+    setIsAddPlacePopupOpen(false);
+    setSelectedCard({});
+  }
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Header />
+
+        <Main
+          cards={cards}
+          onEditAvatar={handleEditAvatarClick}
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddCardClick}
+          onCardClick={handleCardClick}
+          onCardLike={handleCardLike}
+          onCardDelete={handleCardDelete}
+        />
+
+        <Footer />
+        <EditProfilePopup
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser}
+        />
+
+        <EditAvatarPopup
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          onUpdateAvatar={handleUpdateAvatar}
+        />
+        <AddPlacePopup
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          onAddPlace={handleAddPlace}
+        />
+
+        <PopupWithForm title="Вы уверены?" name="remove" buttonTitle="Да" />
+        <ImagePopup card={selectedCard} onClose={closeAllPopups} />
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/utils/Api.js
@@ -0,0 +1,108 @@
+class Api {
+  constructor({ address, token, groupId }) {
+    this._address = address;
+    this._token = token;
+    this._groupId = groupId;
+  }
+
+  
+
+  _checkResponse(result) {
+    return result.ok ? result.json() : Promise.reject(`${result.status}`);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._address}/${this._groupId}/cards`, {
+      headers: {
+        authorization: this._token,
+      },
+    }).then(this._checkResponse);
+  }
+
+  getUserData(data) {
+    return fetch(`${this._address}/${this._groupId}/users/me`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._address}/${this._groupId}/users/me`, {
+      method: 'GET',
+      headers: {
+        authorization: this._token,
+      },
+    }).then(this._checkResponse);
+  }
+
+  addCard(data) {
+    return fetch(`${this._address}/${this._groupId}/cards`, {
+      method: "POST",
+      headers: {
+        authorization: this._token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        link: data.link,
+        name: data.photoName,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  changeAvatar(infoAvatar) {
+    return fetch(`${this._address}/${this._groupId}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        authorization: this._token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        avatar: infoAvatar.avatar,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  deleteCard(id) {
+    return fetch(`${this._address}/${this._groupId}/cards/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: this._token,
+      },
+    }).then(this._checkResponse);
+  }
+
+  setLike(id, isLiked) {
+    const method = isLiked ? "PUT" : "DELETE";
+    return fetch(`${this._address}/${this._groupId}/cards/likes/${id}`, {
+      method: method,
+      headers: {
+        authorization: this._token,
+      },
+    }).then(this._checkResponse);
+  }
+
+  // deleteLike(cardId) {
+  //   return fetch(`${this._address}/${this._groupId}/cards/likes/${cardId}`, {
+  //     method: "DELETE",
+  //     headers: {
+  //       authorization: this._token,
+  //     },
+  //   }).then(this._checkResponse);
+  // }
+}
+
+const api = new Api({
+  address: "https://nomoreparties.co/v1",
+  token: "47630fd7-d22e-4429-9c8d-fa77ac8ebf56",
+  groupId: "cohort-26",
+})
+
+
+export default api;
